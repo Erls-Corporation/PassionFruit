bbc260c12a8667a8d0f4f33a141d3bf7173c7ace
Jake Goldsborough <jake.goldsborough@Iinteractive.com>
2012-09-19 20:16:28 -0400

    deleting old js and fixing positioning bug

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
-  jQuery('#carousel').jcarousel({
-    vertical: false,
-    start: 1,
-    offset: 1,
-    scroll: 1,
-    visible: 1
-  }); 
+  $('.pager li a').click(function() {
+    var $clicked_index = $(this).parent().index();
+    $('.pager li').removeClass('active');
+    $('.pager li').eq($clicked_index).addClass('active');
+
+    // $('#carousel > li').removeClass('active');
+    // $('#carousel > li').eq($clicked_index).addClass('active');
+
+    $('#carousel > li').fadeOut('1000');
+    $('#carousel > li').eq($clicked_index).delay('400').fadeIn('500');
+    // var $content_height = $('#carousel > li').eq($clicked_index).height();
+    // $('#carousel').css('min-height', $content_height);
+    
+    // var $content_height = $('#carousel > li').eq($clicked_index).height();
+    // $('#carousel').css('min-height', $content_height);
+   
+    return false;
+  });
+
+  // var $content_height = $('.content.active').height();
+  // $('#carousel').css('min-height', $content_height);
 });
